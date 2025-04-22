e35c5b527f781a4febff45bc693bb1db2f70f362
aristit0 <aristitosugihharto@gmail.com>
2025-04-23 04:32:21 +0700

    Add master_dag and PySpark scripts for CDE PoC 32

--- employee_pipeline/master_dag.py
+++ employee_pipeline/master_dag.py
@@ -1,22 +1,34 @@
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 from datetime import datetime
-from cloudera.cde.operators.cde_operator import CDEJobRunOperator
+import subprocess
+
+def run_create_stg():
+    subprocess.run(["spark-submit", "/app/mount/employee_pipeline/scripts/create_employee_stg.py"], check=True)
+
+def run_insert_pst():
+    subprocess.run(["spark-submit", "/app/mount/employee_pipeline/scripts/insert_employee_pst.py"], check=True)
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 1, 1),
+}
 
 with DAG(
     dag_id="master_employee_dag",
-    default_args={"owner": "airflow", "start_date": datetime(2024, 1, 1)},
+    default_args=default_args,
     schedule_interval=None,
-    catchup=False,
+    catchup=False
 ) as dag:
 
-    run_create_stg = CDEJobRunOperator(
-        task_id="create_employee_stg",
-        name="create-employee-stg"
+    task_create_stg = PythonOperator(
+        task_id="run_create_employee_stg",
+        python_callable=run_create_stg
     )
 
-    run_insert_pst = CDEJobRunOperator(
-        task_id="insert_employee_pst",
-        name="insert-employee-pst"
+    task_insert_pst = PythonOperator(
+        task_id="run_insert_employee_pst",
+        python_callable=run_insert_pst
     )
 
-    run_create_stg >> run_insert_pst
+    task_create_stg >> task_insert_pst
