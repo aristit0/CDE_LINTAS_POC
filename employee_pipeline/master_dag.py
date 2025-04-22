ab63b4a435f5f9302e301cc7b2dffa8a5d25bfc7
aristit0 <aristitosugihharto@gmail.com>
2025-04-23 04:15:52 +0700

    Add master_dag and PySpark scripts for CDE PoC 2

--- employee_pipeline/master_dag.py
+++ employee_pipeline/master_dag.py
@@ -2,26 +2,21 @@ from airflow import DAG
 from datetime import datetime
 from cloudera.cde.operators.cde_operator import CDEJobRunOperator
 
-default_args = {
-    "owner": "airflow",
-    "start_date": datetime(2024, 1, 1),
-    "depends_on_past": False,
-}
+with DAG(
+    dag_id="master_employee_dag",
+    default_args={"owner": "airflow", "start_date": datetime(2024, 1, 1)},
+    schedule_interval=None,
+    catchup=False,
+) as dag:
 
-with DAG("master_employee_dag",
-         default_args=default_args,
-         schedule_interval=None,
-         catchup=False,
-         description="Run staging and partition insert jobs") as dag:
-
-    create_stg = CDEJobRunOperator(
+    run_create_stg = CDEJobRunOperator(
         task_id="create_employee_stg",
-        name="create-employee-stg",  # harus sesuai dengan job name di CDE
+        name="create-employee-stg"
     )
 
-    insert_pst = CDEJobRunOperator(
+    run_insert_pst = CDEJobRunOperator(
         task_id="insert_employee_pst",
-        name="insert-employee-pst",  # harus sesuai dengan job name di CDE
+        name="insert-employee-pst"
     )
 
-    create_stg >> insert_pst
+    run_create_stg >> run_insert_pst
